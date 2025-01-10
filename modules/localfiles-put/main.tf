377ce01fd8d93d2f4384f471393e7dc1c037c3f5
Tom Nowak <ionfury@gmail.com>
2025-01-10 15:12:06 -0500

    Remove localfiles-put module and associated files

--- modules/localfiles-put/main.tf
+++
@@ -1,7 +0,0 @@
-resource "local_file" "this" {
-  for_each = var.localfiles
-
-  content         = each.value.content
-  filename        = pathexpand(each.value.filename)
-  file_permission = each.value.file_permissions
-}
--- modules/localfiles-put/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 1.6.6"
-  required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "2.1.0"
-    }
-  }
-}
