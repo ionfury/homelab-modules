8e53011647047bc486a5735eb72673e3549e8718
Tom Nowak <ionfury@gmail.com>
2025-01-15 21:19:29 -0500

    Update flux.tf to use dynamic GitHub repository path and add variable for repository path

--- modules/bootstrap/flux.tf
+++ modules/bootstrap/flux.tf
@@ -6,6 +6,6 @@ resource "flux_bootstrap_git" "this" {
   depends_on = [data.github_repository.this]
 
   version                = var.flux_version
-  path                   = "clusters/${var.cluster_name}"
+  path                   = "${var.github_repository_path}/${var.cluster_name}"
   kustomization_override = file("${path.module}/resources/kustomization.yaml")
 }
