8668526ff465487e74b749ad40c9dfe9ab6fb6f3
Tom Nowak <ionfury@gmail.com>
2025-01-24 10:54:24 -0500

    Ability to pass extra disks to talos cluster.

--- modules/talos-cluster/config.tf
+++ modules/talos-cluster/config.tf
@@ -26,6 +26,7 @@ data "talos_machine_configuration" "this" {
       machine_install             = each.value.install
       machine_install_disk_image  = each.value.install.secureboot ? data.talos_image_factory_urls.machine_image_url[each.key].urls.installer_secureboot : data.talos_image_factory_urls.machine_image_url[each.key].urls.installer
       machine_install_files       = concat(each.value.files, var.machine_files)
+      machine_disks               = each.value.disks
       machine_network_nameservers = var.machine_network_nameservers
       machine_time_servers        = var.machine_time_servers
       machine_kubelet_extraMounts = var.machine_kubelet_extraMounts
