7b43b1862f8ee006dd6668ce4c9956e26a629627
Tom Nowak <ionfury@gmail.com>
2025-01-30 23:43:21 -0500

    Will remove these outputs in a subsequent PR

--- modules/talos-cluster/output.tf
+++ modules/talos-cluster/output.tf
@@ -30,17 +30,21 @@ output "kubeconfig_filename" {
 }
 
 output "kubeconfig_host" {
-  value = talos_cluster_kubeconfig.this.kubernetes_client_configuration.host
+  sensitive = true
+  value     = talos_cluster_kubeconfig.this.kubernetes_client_configuration.host
 }
 
 output "kubeconfig_client_certificate" {
-  value = base64decode(talos_cluster_kubeconfig.this.kubernetes_client_configuration.client_certificate)
+  sensitive = true
+  value     = base64decode(talos_cluster_kubeconfig.this.kubernetes_client_configuration.client_certificate)
 }
 
 output "kubeconfig_client_key" {
-  value = base64decode(talos_cluster_kubeconfig.this.kubernetes_client_configuration.client_key)
+  sensitive = true
+  value     = base64decode(talos_cluster_kubeconfig.this.kubernetes_client_configuration.client_key)
 }
 
 output "kubeconfig_cluster_ca_certificate" {
-  value = base64decode(talos_cluster_kubeconfig.this.kubernetes_client_configuration.ca_certificate)
+  sensitive = true
+  value     = base64decode(talos_cluster_kubeconfig.this.kubernetes_client_configuration.ca_certificate)
 }
