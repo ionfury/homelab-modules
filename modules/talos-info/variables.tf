823d27f072f0ecfb17a6de6af1fa6253c0e13570
Tom Nowak <ionfury@gmail.com>
2025-06-11 12:26:02 -0400

    Remove talos info; remove cluster apply.tftest.hcl scaling and upgrade, keep those in cluster-talos

--- modules/talos-info/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "talos_config_path" {
-  description = "The path to the talos config file."
-  type        = string
-}
-
-variable "node" {
-  description = "The node to check."
-  type        = string
-}
